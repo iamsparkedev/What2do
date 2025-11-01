0960c4a6f9dcb191b3576cfab30c706116afc85b
iamsparkedev <sifathasansami2@gmail.com>
2025-11-01 16:05:49 +0600

    Add files

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+// CLI TO-DO APP Without any external libraries
+
+func main() {
+
+    fmt.Println("                     ")
+    fmt.Println("======================== WHAT-2-DO ======================")
+	fmt.Println("                     ")
+	fmt.Println("Welcome to What-2-Do, your simple CLI to-do application!")
+	fmt.Println("You can add, view, and delete your tasks easily.")
+	fmt.Println("                     ")
+	fmt.Println("Available Commands:")
+	fmt.Println("1. add <task>       - Add a new task")
+	fmt.Println("2. view             - View all tasks")
+	fmt.Println("3. delete <taskID>  - Delete a task by its ID")
+	fmt.Println("4. exit             - Exit the application")
+	fmt.Println("                     ")
+	fmt.Println("Let's get started!")
+	fmt.Println("=========================================================")
+
+
+
+}
