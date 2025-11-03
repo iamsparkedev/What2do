55916f96ebd4fed7c655c5f8be0ff997e89e9cca
iamsparkedev <sifathasansami2@gmail.com>
2025-11-03 16:04:38 +0600

    make

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	defer fmt.Println("4. todo exit       - Exit from the application")
 	defer fmt.Println("3. todo delete <taskid>      - Delete a task")
 	defer fmt.Println("2. todo list             - View all tasks")
-	defer fmt.Println("1. todo add <task>       - Add a new task")
+	defer fmt.Println(`1. todo add "<task>"       - Add a new task`)
 	defer fmt.Println("Available Commands:")
 	defer fmt.Println("                     ")
 	defer fmt.Println("Let's get started!")
