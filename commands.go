d72367a9bd1e9b0779c59a84f0cfa95efec3de66
iamsparkedev <sifathasansami2@gmail.com>
2025-12-28 18:55:20 +0600

    Auto-commit: 2025-12-28 18:55:15

--- commands.go
+++ commands.go
@@ -7,8 +7,14 @@ import (
 
 func handleAddTask(task string) {
 	defer fmt.Printf("Task added: %s\n", task)
-	
-
+    filename := "task.txt"
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Printf("Error opening file: %v\n", err)
+		return
+	}
+	defer file.Close()
+	os.WriteFile(filename, []byte(task), 0644)
 }
 
 func handleListTasks() {
